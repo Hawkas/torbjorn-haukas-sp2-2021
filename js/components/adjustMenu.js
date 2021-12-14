108d91696ab494cece7c084781bcb8df2f0b5f83
Torbjørn Haukås <69842749+Hawkas@users.noreply.github.com>
2021-12-15 00:28:12 +0100

    okay and we're off

--- js/components/adjustMenu.js
+++ js/components/adjustMenu.js
@@ -1,20 +1,39 @@
-import { getUser } from "../libs/localStorageHelper.js";
-import { signOut } from "../libs/utilityFunctions.js";
+// import { getUser } from "../libs/localStorageHelper.js";
+// import { signOut } from "../libs/utilityFunctions.js";
+
+// const adjustMenu = function () {
+// 	const navbar = document.querySelector(".navigation__right");
+// 	let signOutBtn = {};
+// 	if (getUser("jwt") !== null) {
+// 		navbar.innerHTML += `
+//         <div class="nav-item ms-auto">
+//             <a class="btn btn-outline-danger btn-sm sign-out" role="button" href="">Sign out</a>
+//         </div>`;
+// 		signOutBtn = document.querySelector(".sign-out");
+// 		signOutBtn.addEventListener("click", signOut);
+// 	} else {
+// 		navbar.innerHTML += `
+//         <div class="nav-item ms-auto">
+// 			<a class="btn btn-outline-primary btn-sm" href="./login.html">Sign in</a>
+// 		</div>
+//         `;
+// 	}
+// };
+// export default adjustMenu;
 
 const adjustMenu = function () {
-	const navbar = document.querySelector(".navbar-nav");
-	let signOutBtn = {};
-	if (getUser("jwt") !== null) {
-		navbar.innerHTML += `
-        <li class="nav-item ms-auto">
-            <a class="nav-link btn btn-outline-danger sign-out" role="button" href="">Sign out</a>
-        </li>`;
-		signOutBtn = document.querySelector(".sign-out");
-		signOutBtn.addEventListener("click", signOut);
-	} else {
-		navbar.innerHTML += `
-        <li class="nav-item ms-auto"><a class="nav-link btn btn-outline-info" href="./login.html">Sign in</a></li>
-        `;
-	}
+	const hamburger = document.querySelector(".hamburger");
+	const menuDivider = document.querySelector(".navigation__divider");
+	const fullNav = document.querySelector(".navigation");
+	hamburger.addEventListener("click", function () {
+		this.classList.toggle("is-active");
+		menuDivider.classList.toggle("is-active");
+		fullNav.classList.toggle("is-active");
+	});
+	let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-tooltip="tooltip"]'));
+	let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl);
+	});
 };
+
 export default adjustMenu;
